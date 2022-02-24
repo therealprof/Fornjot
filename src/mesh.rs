9e45b628eebd860a156d81b6c1f6aef970c53409
Daniel Egger <daniel@eggers-club.de>
2022-02-25 00:22:17 +0100

    Don't attempt to hash vertices
    Signed-off-by: Daniel Egger <daniel@eggers-club.de>

--- src/mesh.rs
+++ src/mesh.rs
@@ -1,36 +1,31 @@
-use std::{collections::HashMap, hash::Hash};
-
 /// API for creating a mesh
 pub struct MeshMaker<V> {
     vertices: Vec<V>,
     indices: Vec<Index>,
-
-    indices_by_vertex: HashMap<V, Index>,
 }
 
 impl<V> MeshMaker<V>
 where
-    V: Copy + Eq + Hash,
+    V: Copy + Eq,
 {
     /// Create a new instance of `MeshMaker`
     pub fn new() -> Self {
         Self {
             vertices: Vec::new(),
             indices: Vec::new(),
-            indices_by_vertex: HashMap::new(),
         }
     }
 
     /// Add a vertex to the mesh
     pub fn push(&mut self, vertex: V) {
-        let index =
-            *self.indices_by_vertex.entry(vertex).or_insert_with(|| {
-                let index = self.vertices.len();
-                self.vertices.push(vertex);
-                index as u32
-            });
+        let pos = self.vertices.iter().position(|&v| v == vertex);
+        let index = pos.unwrap_or_else(|| {
+            let index = self.vertices.len();
+            self.vertices.push(vertex);
+            index
+        });
 
-        self.indices.push(index);
+        self.indices.push(index as u32);
     }
 
     /// Access the vertices of the mesh
